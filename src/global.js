1522eb0ff73f531c0586645a33502fe3e46dde66
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-10-06 21:50:37 +0530

    updated heroku URL

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 
-export const API = "https://best-movies-blog-backend.herokuapp.com/";
+export const API = "https://best-movies-blog-backend.herokuapp.com";
 // export const API = "http://localhost:4000";
