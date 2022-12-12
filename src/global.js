1d57cb78d7145f7e7cdb41aaf33d7acd1d1c21b2
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-12-12 22:41:35 +0530

    added render deployed link

--- src/global.js
+++ src/global.js
@@ -1,3 +1,5 @@
 
-export const API = "https://best-movies-blog-backend.herokuapp.com";
+// export const API = "https://best-movies-blog-backend.herokuapp.com";
 // export const API = "http://localhost:4000";
+
+export const API = "https://best-movies-blog-backend.onrender.com"
